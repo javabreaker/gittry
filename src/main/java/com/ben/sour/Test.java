33de0c97ac50c295140222201d906926f59b9555
Ben Zhang <ben.zhang@sunbasket.com>
2019-03-08 21:57:26 -0600

    master 2

--- src/main/java/com/ben/sour/Test.java
+++ src/main/java/com/ben/sour/Test.java
@@ -3,6 +3,7 @@ package com.ben.sour;
 public class Test {
 
 	public void doSomething() {
-		//this is line in the master:
+		//this is line 1 in the master:
+		//this is line 2 in the master:
 	}
 }
